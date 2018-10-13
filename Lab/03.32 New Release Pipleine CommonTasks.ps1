7736a6476e26355224e41f040376661536493875
Raimund Andée [MSFT] <r.andree@live.com>
2018-10-13 17:03:24 +0200

    More cleanup, changing the order and fixing project name

--- Lab/03.32 New Release Pipleine CommonTasks.ps1
+++ Lab/03.32 New Release Pipleine CommonTasks.ps1
@@ -3,10 +3,18 @@ $tfsServer = Get-LabVM -Role Tfs2018
 $tfsCred = $tfsServer.GetCredential($lab)
 $tfsPort = 8080
 
-$projectName = 'CommonTasks2'
+$projectName = 'CommonTasks'
 $projectGitUrl = 'https://github.com/AutomatedLab/CommonTasks'
 $collectionName = 'AutomatedLab'
 
+# Which will make use of TFS, clone the stuff, add the necessary build step, publish the test results and so on
+# You will see two remotes, Origin (Our code on GitHub) and TFS (Our code pushed to your lab)
+Write-ScreenInfo 'Creating TFS project and cloning from GitHub...' -NoNewLine
+
+New-LabReleasePipeline -ProjectName $projectName -SourceRepository $projectGitUrl -CodeUploadMethod FileCopy
+$tfsAgentQueue = Get-TfsAgentQueue -InstanceName $tfsServer -Port $tfsPort -Credential $tfsCred -ProjectName $projectName -CollectionName $collectionName -QueueName Default
+
+#region Build and Release Definitions
 # Create a new release pipeline
 # Get those build steps from Get-LabBuildStep
 $buildSteps = @(
@@ -21,8 +29,6 @@ $buildSteps = @(
         }
         "inputs"          = @{
             targetType          = "inline"
-            scriptName          = ""
-            arguments           = ""
             script              = @'
 #always make sure the local PowerShell Gallery is registered correctly
 $uri = '$(GalleryUri)'
@@ -35,30 +41,23 @@ if (-not $r -or $r.SourceLocation -ne $uri -or $r.PublishLocation -ne $uri) {
     Get-PSRepository
 }
 '@
-            failOnStderr = $false
-            errorActionPreference = 'stop'
         }
     }
     @{
         "enabled"         = $true
-        "continueOnError" = $false
-        "alwaysRun"       = $false
         "displayName"     = "Execute Build.ps1"
         "task"            = @{
             "id"          = "e213ff0f-5d5c-4791-802d-52ea3e7be1f1"
-            "versionSpec" = "*"
+            "versionSpec" = "2.*"
         }
         "inputs"          = @{
-            scriptType          = "filePath"
-            scriptName          = "Build.ps1"
+            targetType          = "filePath"
+            filePath            = "Build.ps1"
             arguments           = '-ResolveDependency -GalleryRepository PowerShell -Tasks ClearBuildOutput, Init, SetPsModulePath, CopyModule, IntegrationTest'
-            failOnStandardError = $false
         }
     }
     @{
         enabled         = $true
-        continueOnError = $false
-        alwaysRun       = $false
         displayName     = 'Publish Integration Test Results'
         condition       = 'always()'
         task            = @{
@@ -74,8 +73,6 @@ if (-not $r -or $r.SourceLocation -ne $uri -or $r.PublishLocation -ne $uri) {
     }
     @{
         enabled         = $true
-        continueOnError = $false
-        alwaysRun       = $false
         displayName     = 'Publish Artifact: $(Build.Repository.Name) Module'
         task            = @{
             id          = '2ff763a7-ce83-4e1f-bc89-0ae63477cebe'
@@ -89,8 +86,6 @@ if (-not $r -or $r.SourceLocation -ne $uri -or $r.PublishLocation -ne $uri) {
     }
     @{
         enabled         = $true
-        continueOnError = $false
-        alwaysRun       = $false
         displayName     = 'Publish Artifact: BuildFolder'
         task            = @{
             id          = '2ff763a7-ce83-4e1f-bc89-0ae63477cebe'
@@ -272,17 +267,12 @@ $releaseEnvironments = @(
         }
     }
 )
+#endregion
 
-# Which will make use of TFS, clone the stuff, add the necessary build step, publish the test results and so on
-# You will see two remotes, Origin (Our code on GitHub) and TFS (Our code pushed to your lab)
-Write-ScreenInfo 'Creating TFS project and cloning from GitHub...' -NoNewLine
-
-New-LabReleasePipeline -ProjectName $projectName -SourceRepository $projectGitUrl -CodeUploadMethod FileCopy
-$tfsAgentQueue = Get-TfsAgentQueue -InstanceName $tfsServer -Port $tfsPort -Credential $tfsCred -ProjectName $projectName -CollectionName $collectionName -QueueName Default
 $repo = Get-TfsGitRepository -InstanceName $tfsServer -Port 8080 -CollectionName $collectionName -ProjectName $projectName -Credential $tfsCred
 $refs = (Invoke-RestMethod -Uri "http://$($tfsServer):$tfsPort/$collectionName/_apis/git/repositories/{$($repo.id)}/refs?api-version=4.1" -Credential $tfsCred).value.name
 
-New-TfsBuildDefinition -ProjectName $projectName -InstanceName $tfsServer -Port $tfsPort -DefinitionName "$($projectName)Build" -CollectionName $collectionName -BuildTasks $buildSteps -Variables @{ GalleryUri = 'http://dscpull01.contoso.com/nuget/PowerShell' } -CiTriggerRefs $refs -Credential $tfsCred #-ApiVersion 4.1
+New-TfsBuildDefinition -ProjectName $projectName -InstanceName $tfsServer -Port $tfsPort -DefinitionName "$($projectName)Build" -CollectionName $collectionName -BuildTasks $buildSteps -Variables @{ GalleryUri = 'http://dscpull01.contoso.com/nuget/PowerShell' } -CiTriggerRefs $refs -Credential $tfsCred -ApiVersion 4.1
 
 New-TfsReleaseDefinition -ProjectName $projectName -InstanceName $tfsServer -Port $tfsPort -ReleaseName "$($projectName)Release" -Environments $releaseEnvironments -Credential $tfsCred -CollectionName $collectionName #-ApiVersion '4.1-preview'
 Write-ScreenInfo done
