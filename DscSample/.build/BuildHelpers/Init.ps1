130785a24e065bf5159f60805d8abe62c81b72d9
Raimund Andrée [MSFT] <r.andree@live.com>
2018-10-14 10:42:10 +0200

    Removing debug output

--- DscSample/.build/BuildHelpers/Init.ps1
+++ DscSample/.build/BuildHelpers/Init.ps1
@@ -1,6 +1,5 @@
 Task Init {
-1
-Get-Location
+
     if (-not $env:BHProjectName) {
         try {
             Set-BuildEnvironment -Path $ProjectPath
@@ -10,13 +9,11 @@ Get-Location
             Write-Host "Error calling 'Set-BuildEnvironment'. The task will probably fail if in build."
         }
     }
-2
-Get-Location
+
     $lines
     Set-Location -Path $ProjectPath
     "Build System Details:"
     Get-Item -Path env:BH*
     "`n"
-    3
-    Get-Location
+    
 }
