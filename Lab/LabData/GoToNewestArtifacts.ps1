084ff1098f329779683e96b8ba26bd90ad6dbec4
Raimund Andée [MSFT] <r.andree@live.com>
2018-10-22 21:19:09 +0800

    Adding two helper files to the TFS Desktop

---
+++ Lab/LabData/GoToNewestArtifacts.ps1
@@ -0,0 +1,6 @@
+﻿$latestBuild = dir C:\Artifacts\DscWorkshopBuild | Sort-Object -Property { [int]$_.Name } -Descending | Select-Object -First 1
+
+#cd "$($latestBuild.FullName)\DscWorkshop\MetaMof"
+#cd "$($latestBuild.FullName)\DscWorkshop\MOF"
+
+start "$($latestBuild.FullName)\DscWorkshop"
---
+++ Lab/LabData/UpdateNodes.ps1
@@ -0,0 +1,5 @@
+﻿$computers = Get-ADComputer -Filter { Name -like 'DSCWeb*' -or Name -like 'DSCFile*' } | Select-Object -ExpandProperty DNSHostName
+
+Update-DscConfiguration -ComputerName $computers -Verbose -Wait
+
+Start-DscConfiguration -UseExisting -Wait -Verbose -Force -ComputerName $computers
