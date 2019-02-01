7c11904c632738790b39c9d1f8ef02c5ff1d5158
Raimund Andrée [MSFT] <r.andree@live.com>
2019-02-01 17:34:26 +0100

    Revert back to Datum 0.0.35

--- DscSample/RootMetaMof.ps1
+++ DscSample/RootMetaMof.ps1
@@ -5,35 +5,35 @@ Configuration RootMetaMOF {
         $LcmConfig = $(Lookup 'LcmConfig\Settings' $Null)
         #If the Nodename is a GUID, use Config ID instead Named config, as per SMB Pull requirements
         if($Node.Nodename -as [Guid]) {$LcmConfig['ConfigurationID'] = $Node.Nodename}
-        x Settings '' $LcmConfig
+        Get-DscSplattedResource Settings '' $LcmConfig
 
         if($ConfigurationRepositoryShare = $(Lookup 'LcmConfig\ConfigurationRepositoryShare' $Null)) {
-            x ConfigurationRepositoryShare ConfigurationRepositoryShare $ConfigurationRepositoryShare
+            Get-DscSplattedResource ConfigurationRepositoryShare ConfigurationRepositoryShare $ConfigurationRepositoryShare
         }
 
         if($ResourceRepositoryShare = $(Lookup 'LcmConfig\ResourceRepositoryShare' $Null)) {
-            x ResourceRepositoryShare ResourceRepositoryShare $ResourceRepositoryShare
+            Get-DscSplattedResource ResourceRepositoryShare ResourceRepositoryShare $ResourceRepositoryShare
         }
 
         if($ConfigurationRepositoryWeb = $(Lookup 'LcmConfig\ConfigurationRepositoryWeb' $Null)) {
             foreach($ConfigRepoName in $ConfigurationRepositoryWeb.keys) {
-                x ConfigurationRepositoryWeb $ConfigRepoName $ConfigurationRepositoryWeb[$ConfigRepoName]
+                Get-DscSplattedResource ConfigurationRepositoryWeb $ConfigRepoName $ConfigurationRepositoryWeb[$ConfigRepoName]
             }
         }
 
         if($ResourceRepositoryWeb = $(Lookup 'LcmConfig\ResourceRepositoryWeb' $Null)) {
             foreach($ResourceRepoName in $ResourceRepositoryWeb.keys) {
-                x ResourceRepositoryWeb $ResourceRepoName $ResourceRepositoryWeb[$ResourceRepoName]
+                Get-DscSplattedResource ResourceRepositoryWeb $ResourceRepoName $ResourceRepositoryWeb[$ResourceRepoName]
             }
         }
 
         if($ReportServerWeb = $(Lookup 'LcmConfig\ReportServerWeb' $Null)) {
-            x ReportServerWeb ReportServerWeb $ReportServerWeb
+            Get-DscSplattedResource ReportServerWeb ReportServerWeb $ReportServerWeb
         }
 
         if($PartialConfiguration = $(Lookup 'LcmConfig\PartialConfiguration' $Null)) {
             foreach($PartialConfigurationName in $PartialConfiguration.keys) {
-                x PartialConfiguration $PartialConfigurationName $PartialConfiguration[$PartialConfigurationName]
+                Get-DscSplattedResource PartialConfiguration $PartialConfigurationName $PartialConfiguration[$PartialConfigurationName]
             }
         }
     }
