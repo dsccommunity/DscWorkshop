9c9ffcf274b6969aeb3821a161c560d7a58ebabf
gaelcolas <gaelcolas@synedgy.com>
2019-02-05 08:54:10 +0800

    adding a load of xPSDesiredStateConfiguration before the config

--- DscSample/.build/Tasks/CompileRootConfiguration.ps1
+++ DscSample/.build/Tasks/CompileRootConfiguration.ps1
@@ -2,13 +2,14 @@ task CompileRootConfiguration {
 
     $tid = [System.Threading.Thread]::CurrentThread.ManagedThreadId
     Start-Transcript -Path "$BuildOutput\Logs\Compile_Root_Configuration$tid-Log.txt"
+    Import-Module xPSDesiredStateConfiguration
 
     try
     {
         $mofs = . (Join-Path -Path $ProjectPath -ChildPath 'RootConfiguration.ps1')
         Write-Build Green "Successfully compiled $($mofs.Count) MOF files"
     }
-    catch 
+    catch
     {
         Write-Build Red "ERROR OCCURED DURING COMPILATION: $($_.Exception.Message)"
         $relevantErrors = $Error | Where-Object {
@@ -18,5 +19,5 @@ task CompileRootConfiguration {
     }
 
     Stop-Transcript
-    
+
 }
