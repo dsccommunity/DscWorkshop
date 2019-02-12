6d522fb838bd601cfee7da864bb9e35feabff7d8
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2019-02-12 12:33:23 -0800

    Added prereq check

---
+++ Exercises/CheckPrereq.ps1
@@ -0,0 +1,33 @@
+#requires -Module @{ModuleName = "Pester"; ModuleVersion="4.0"}
+
+describe "Prerequisite check" {
+
+    Context "Task 1" {
+        it "Should have git installed and in path" {
+            Get-Command git -ea SilentlyContinue | Should -Not -Be $null -Because "We require git for many lab exercises"
+        }
+
+        it "Should not be executed on PowerShell Core" {
+            $PSVersionTable.PSVersion -lt '6.0' | Should -Be $true -Because "Currently the build process is not capable of running with PowerShell Core"
+        }
+
+        it "Should be using VS Code" {
+            Get-Command code.cmd -ErrorAction SilentlyContinue | Should -Not -Be $null
+        }
+
+        it "Should have PowerShell and yaml extension installed" {
+            (Get-ChildItem -Path $home\.vscode\extensions).Name -Match "ms-vscode\.powershell|redhat\.vscode-yaml" | Should -Not -Be $null
+        }
+    }
+
+    Context "Task 2" {
+        it "Should have Az module installed" {
+            (Get-Module -List Az.*).Count | Should -BeGreaterOrEqual 45 -Because "We need the module Az installed with all its components"
+        }
+
+        it "Should be logged in to Azure" {
+            {Get-AzContext} | Should -Not -Throw
+            Get-AzContext | Should -Not -Be $null
+        }
+    }
+}
--- Exercises/README.md
+++ Exercises/README.md
@@ -2,6 +2,16 @@
 
 These small exercises should get you started with this repository and the project. Use these little tasks as a guideline, and be sure to replace settings with your own ones to improve your learning experience.
 
+Before starting any exercise, please execute the [prerequisite check](CheckPrereq.ps1) in Windows PowerShell. ***Please not that this test requires Pester > 4 to run.***
+
+## Prerequisites
+
+- [git](https://git-scm.org)
+- [VSCode](https://code.visualstudio.com)
+- [Azure DevOps account (free)](https://dev.azure.com)
+- Optional: Access to an [Azure](https://portal.azure.com) subscription
+  - required to test Azure Automation DSC in a release pipeline
+
 ## Task 1
 
 Task 1 is all about getting around the build environment step by step. From running a manual build to easily modifying the entire environment without modifying the actual DSC code you can experience everything.  
