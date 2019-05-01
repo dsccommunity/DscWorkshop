a1a6ff6131c9ce22bbc41b32fcf2ce776d808848
Raimund Andrée [MSFT] <r.andree@live.com>
2019-05-01 22:41:08 +0200

    Pull Server Deployment Acceptance tests now consideres the build number instead build time

--- DscSample/Tests/Acceptance/TestMofFiles.Tests.ps1
+++ DscSample/Tests/Acceptance/TestMofFiles.Tests.ps1
@@ -6,8 +6,6 @@ $environments = (Get-ChildItem $here\..\..\DSC_ConfigData\AllNodes -Directory).B
 $roleDefinitions = Get-ChildItem $here\..\..\DSC_ConfigData\Roles -Recurse -Include *.yml
 $datum = New-DatumStructure -DefinitionFile $datumDefinitionFile
 $configurationData = Get-FilteredConfigurationData -Environment $environment -Datum $datum -Filter $filter
-$buildStartTime = [datetime]$env:BHBuildStartTime
-#$buildOutput = $env:BHBuildOutput
 
 $nodeNames = [System.Collections.ArrayList]::new()
 
@@ -18,7 +16,7 @@ Describe 'Pull Server Deployment' -Tag BuildAcceptance, PullServer {
     foreach ($node in $environmentNodes) {
         It "MOF file for node $($node.NodeName) was deployed to $($env:DscConfiguration)" {
         
-            Get-ChildItem -Path $env:DscConfiguration -Filter "$($node.NodeName).mof" | Where-Object LastWriteTime -gt $buildStartTime | Should -Not -BeNullOrEmpty
+            Get-ChildItem -Path $env:DscConfiguration -Filter "$($node.NodeName).mof" | Select-String -Pattern ">>$($env:BHBuildNumber)<<" | Should -Not -BeNullOrEmpty
 
         }
     }
