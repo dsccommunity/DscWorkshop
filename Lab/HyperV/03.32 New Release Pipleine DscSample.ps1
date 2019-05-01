09ebea84a7e3ae900b28e0d619b796a50d3488c3
Raimund Andrée [MSFT] <r.andree@live.com>
2019-05-01 22:39:09 +0200

    Adding checkpoints after pipeline build

--- Lab/HyperV/03.32 New Release Pipleine DscSample.ps1
+++ Lab/HyperV/03.32 New Release Pipleine DscSample.ps1
@@ -595,3 +595,7 @@ $releaseParameters = @{
 New-TfsReleaseDefinition @releaseParameters
 
 Write-ScreenInfo done
+
+# in case you screw something up
+Checkpoint-LabVM -All -SnapshotName AfterPipelines
+Write-Host "3. - Creating Snapshot 'AfterPipeline'" -ForegroundColor Magenta
