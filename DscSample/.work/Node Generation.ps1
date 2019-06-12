21ded9504b82f761e879b2eec5b0599e8257be76
Raimund Andrée [MSFT] <r.andree@live.com>
2019-06-12 16:41:56 +0200

    Adding debug script

---
+++ DscSample/.work/Node Generation.ps1
@@ -0,0 +1,29 @@
+﻿$yaml = @"
+NodeName: {0}
+Environment: Dev
+Role: {2}
+Description: File Server in Dev
+Location: Frankfurt
+
+NetworkIpConfiguration:
+  IpAddress: 192.168.111.{1}
+  Prefix: 24
+  Gateway: 192.168.111.50
+  DnsServer: 192.168.111.10
+  InterfaceAlias: Ethernet
+  DisableNetbios: True
+
+PSDscAllowPlainTextPassword: True
+PSDscAllowDomainUser: True
+
+LcmConfig:
+  ConfigurationRepositoryWeb:
+    Server:
+      ConfigurationNames: {0}
+"@
+
+1..100 | ForEach-Object {
+    $nodeName = 'DSCFile{0:D4}' -f $_
+    $newYaml = $yaml -f $nodeName, $_, 'FileServer' 
+    $newYaml | Out-File -FilePath "$PSScriptRoot\..\DSC_ConfigData\AllNodes\Dev\$nodeName.yml"
+}
