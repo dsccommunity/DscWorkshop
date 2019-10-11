3eb9f6862d3af6c5912d20fe5a0f1903b415f78f
Raimund Andree <r.andree@live.com>
2019-10-12 00:29:00 +0200

    Fixing MacAddressPrefix and how test VM is deployed

--- Lab/50 HyperV Host.ps1
+++ Lab/50 HyperV Host.ps1
@@ -12,6 +12,8 @@ Invoke-LabCommand -ActivityName 'Install AutomatedLab and create LabSources fold
     Enable-LabHostRemoting -Force
 
     New-LabSourcesFolder -ErrorAction Stop
+    
+    Set-PSFConfig -Name MacAddressPrefix -Module AutomatedLab -Value 1017FB -PassThru | Register-PSFConfig -Scope UserDefault
 }
 
 Install-LabWindowsFeature -ComputerName $hosts -FeatureName RSAT-AD-Tools
@@ -19,22 +21,29 @@ Install-LabWindowsFeature -ComputerName $hosts -FeatureName RSAT-AD-Tools
 Copy-LabFileItem -Path $labSources\ISOs\en_windows_server_2019_x64_dvd_4cb967d8.iso -DestinationFolderPath C:\LabSources\ISOs -ComputerName DSCHost01
 
 Invoke-LabCommand -ActivityName 'Setup Test VM' -ComputerName $hosts -ScriptBlock {
+    [System.Environment]::SetEnvironmentVariable('AUTOMATEDLAB_TELEMETRY_OPTOUT', '0')
     Import-Module -Name AutomatedLab
     $dc = Get-ADDomainController
-    $ip = Get-NetIPAddress -InterfaceAlias Ethernet -AddressFamily IPv4
-    $network = [AutomatedLab.IPNetwork]"$($ip.IPAddress)/$($ip.PrefixLength)"
-
+        
+    $netAdapter = Get-NetAdapter -Name 'vEthernet (AlExternal)' -ErrorAction SilentlyContinue
+    if (-not $netAdapter)
+    {
+        $netAdapter = Get-NetAdapter -Name Ethernet -ErrorAction SilentlyContinue
+    }
+        
+    $ip = $netAdapter | Get-NetIPAddress -AddressFamily IPv4
+    
     #--------------------------------
-
+    
     New-LabDefinition -Name Lab1 -DefaultVirtualizationEngine HyperV
     $os = Get-LabAvailableOperatingSystem | Where-Object { $_.OperatingSystemName -like '*Datacenter*' -and $_.OperatingSystemName -like '*2019*' -and $_.OperatingSystemName -like '*Desktop*' }
-
-    Add-LabVirtualNetworkDefinition -Name Lab1 -AddressSpace "$($network.IpAddress)/$($network.Cidr)" -HyperVProperties @{ SwitchType = 'External'; AdapterName = 'Ethernet' }
-
-    Add-LabMachineDefinition -Name $dc.Name -Memory 1GB -OperatingSystem $os -Roles RootDC -DomainName $dc.Domain -SkipDeployment
-    Add-LabMachineDefinition -Name Client1 -Memory 1GB -OperatingSystem $os #-DomainName $dc.Domain
-
+        
+    Add-LabVirtualNetworkDefinition -Name AlExternal -AddressSpace "$($ip.IPAddress)/$($ip.PrefixLength)" -HyperVProperties @{ SwitchType = 'External'; AdapterName = 'Ethernet' }
+        
+    Add-LabMachineDefinition -Name $dc.Name -Memory 1GB -OperatingSystem $os -Roles RootDC -DomainName $dc.Domain -IpAddress $dc.IPv4Address -SkipDeployment
+    Add-LabMachineDefinition -Name TestVM1 -Memory 1GB -OperatingSystem $os -DomainName $dc.Domain -DnsServer1 $dc.IPv4Address -Gateway (Get-NetIPConfiguration).IPv4DefaultGateway.NextHop
+        
     Install-Lab
-
+        
     Show-LabDeploymentSummary -Detailed
 }
