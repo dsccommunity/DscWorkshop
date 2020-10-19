0562cb133095d791d7112f12adccbe3866fb3a3e
Raimund Andrée <r.andree@live.com>
2020-10-19 13:52:31 +0200

    Added function 'Start-LabDscConfiguration' to helper module

--- Lab/LabData/Helpers.psm1
+++ Lab/LabData/Helpers.psm1
@@ -106,5 +106,11 @@ function Clear-LabBuildWorkers {
 function Test-LabDscConfiguration {
     $computers = Get-ADComputer -Filter { Name -like 'DSCWeb*' -or Name -like 'DSCFile*' } | Select-Object -ExpandProperty DNSHostName
     
-    Test-DscConfiguration -ComputerName $computers -Detailed   
+    Test-DscConfiguration -ComputerName $computers -Detailed -Verbose
+}
+
+function Start-LabDscConfiguration {
+    $computers = Get-ADComputer -Filter { Name -like 'DSCWeb*' -or Name -like 'DSCFile*' } | Select-Object -ExpandProperty DNSHostName
+    
+    Start-DscConfiguration -ComputerName $computers -UseExisting -Wait -Verbose   
 }
