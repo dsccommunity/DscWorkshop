cde8c7fb77c0afad6100c5e2e37ef48c4114eefc
Raimund Andree <r.andree@live.com>
2021-10-16 11:51:55 +0200

    Added logging

--- DSC/Build/Tasks/CompressModulesWithChecksum.ps1
+++ DSC/Build/Tasks/CompressModulesWithChecksum.ps1
@@ -6,38 +6,45 @@ task CompressModulesWithChecksum {
         return
     }
 
-    if (-not (Test-Path -Path $BuildOutput\CompressedModules)) {
-        mkdir -Path $BuildOutput\CompressedModules | Out-Null
-    }
+    Start-Transcript -Path "$BuildOutput\Logs\CompressModulesWithChecksum.log"
 
-    if ($SkipCompressedModulesBuild)
-    {
-        Write-Host 'Skipping preparation of Compressed Modules as $SkipCompressedModulesBuild is set'
-        return
-    }
+    try {
 
-    if ($configurationData.AllNodes -and $CurrentJobNumber -eq 1) {
-        
-        $modules = Get-ModuleFromFolder -ModuleFolder "$ProjectPath\DscResources\"
-        $compressedModulesPath = "$BuildOutput\CompressedModules"
+        if (-not (Test-Path -Path $BuildOutput\CompressedModules)) {
+            mkdir -Path $BuildOutput\CompressedModules | Out-Null
+        }
 
-        foreach ($module in $modules) {
-            $destinationPath = Join-Path -Path $compressedModulesPath -ChildPath "$($module.Name)_$($module.Version).zip"
-            Compress-Archive -Path "$($module.ModuleBase)\*" -DestinationPath $destinationPath
-            $hash = (Get-FileHash -Path $destinationPath).Hash
-        
-            try {
-                $stream = New-Object -TypeName System.IO.StreamWriter("$destinationPath.checksum", $false)
-                [void] $stream.Write($hash)
-            }
-            finally {
-                if ($stream) {
-                    $stream.Close()
+        if ($SkipCompressedModulesBuild)
+        {
+            Write-Host "Skipping preparation of Compressed Modules as '`$SkipCompressedModulesBuild' is set"
+            return
+        }
+        if ($configurationData.AllNodes -and $CurrentJobNumber -eq 1) {
+            
+            $modules = Get-ModuleFromFolder -ModuleFolder "$ProjectPath\DscResources\"
+            $compressedModulesPath = "$BuildOutput\CompressedModules"
+
+            foreach ($module in $modules) {
+                $destinationPath = Join-Path -Path $compressedModulesPath -ChildPath "$($module.Name)_$($module.Version).zip"
+                Compress-Archive -Path "$($module.ModuleBase)\*" -DestinationPath $destinationPath
+                $hash = (Get-FileHash -Path $destinationPath).Hash
+            
+                try {
+                    $stream = New-Object -TypeName System.IO.StreamWriter("$destinationPath.checksum", $false)
+                    [void] $stream.Write($hash)
+                }
+                finally {
+                    if ($stream) {
+                        $stream.Close()
+                    }
                 }
             }
         }
+        else {
+            Write-Build Green "No data in 'ConfigurationData.AllNodes', skipping task 'CompressModulesWithChecksum'."
+        }
     }
-    else {
-        Write-Build Green "No data in 'ConfigurationData.AllNodes', skipping task 'CompressModulesWithChecksum'."
-    }
+    finally {
+        Stop-Transcript
+    } 
 }
