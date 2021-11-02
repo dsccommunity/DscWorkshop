3547e6146de8824ffab3d80f1589c21d785a919d
Raimund Andree <r.andree@live.com>
2021-11-02 23:53:29 +0100

    Updated lab scripts according to sampler update

--- Lab/31 New Release Pipeline CommonTasks.ps1
+++ Lab/31 New Release Pipeline CommonTasks.ps1
@@ -1,18 +1,26 @@
-﻿if (-not (Get-Lab -ErrorAction SilentlyContinue).Name -eq 'DscWorkshop') {
+﻿if (-not (Get-Lab -ErrorAction SilentlyContinue).Name -eq 'DscWorkshop')
+{
     Import-Lab -Name DscWorkshop -NoValidation -ErrorAction Stop
 }
 
-$projectGitUrl = 'https://github.com/raandree/CommonTasks2'
+$projectGitUrl = 'https://github.com/raandree/CommonTasks'
 $projectName = $projectGitUrl.Substring($projectGitUrl.LastIndexOf('/') + 1)
 $collectionName = 'AutomatedLab'
 $lab = Get-Lab
 $devOpsServer = Get-LabVM -Role AzDevOps
 $devOpsWorker = Get-LabVM -Role HyperV
-$devOpsHostName = if ($lab.DefaultVirtualizationEngine -eq 'Azure') { $devOpsServer.AzureConnectionInfo.DnsName } else { $devOpsServer.FQDN }
+$devOpsHostName = if ($lab.DefaultVirtualizationEngine -eq 'Azure')
+{
+    $devOpsServer.AzureConnectionInfo.DnsName
+}
+else
+{
+    $devOpsServer.FQDN
+}
 $nugetServer = Get-LabVM -Role AzDevOps
 $nugetFeed = Get-LabTfsFeed -ComputerName $nugetServer -FeedName PowerShell
 
-$devOpsRole = $devOpsServer.Roles | Where-Object Name -eq AzDevOps
+$devOpsRole = $devOpsServer.Roles | Where-Object Name -EQ AzDevOps
 $devOpsCred = $devOpsServer.GetCredential($lab)
 $devOpsPort = $originalPort = 8080
 if ($devOpsRole.Properties.ContainsKey('Port'))
@@ -35,27 +43,31 @@ Invoke-LabCommand -ActivityName 'Bootstrap NuGet.exe' -ComputerName $devOpsServe
     $nugetPathAllUsers = "$([System.Environment]::GetFolderPath('CommonApplicationData'))\Microsoft\Windows\PowerShell\PowerShellGet\NuGet.exe"
     $nugetPathCurrentUser = "$([System.Environment]::GetFolderPath('LocalApplicationData'))\Microsoft\Windows\PowerShell\PowerShellGet\NuGet.exe"
 
-    if (-not (Test-Path -Path $nugetPath)) {
+    if (-not (Test-Path -Path $nugetPath))
+    {
         mkdir -Path $nugetPath
     }
 
-    $hasNuget = if (Test-Path -Path $nugetPathAllUsers) {
-
+    $hasNuget = if (Test-Path -Path $nugetPathAllUsers)
+    {
         $nugetExe = Get-Item -Path $nugetPathAllUsers
         Write-Host "'nuget.exe' exist in '$nugetPathAllUsers' with version '$($nugetExe.VersionInfo.FileVersionRaw)'"
 
-        if ($nugetExe.VersionInfo.FileVersionRaw -gt '5.11') {
+        if ($nugetExe.VersionInfo.FileVersionRaw -gt '5.11')
+        {
             $true
         }
     }
 
-    if (-not $hasNuget) {
-        if (Test-Path -Path $nugetPathCurrentUser) {
-
+    if (-not $hasNuget)
+    {
+        if (Test-Path -Path $nugetPathCurrentUser)
+        {
             $nugetExe = Get-Item -Path $nugetPathCurrentUser
             Write-Host "'nuget.exe' exist in '$nugetPathCurrentUser' with version '$($nugetExe.VersionInfo.FileVersionRaw)'"
 
-            if ($nugetExe.VersionInfo.FileVersionRaw -gt '5.11') {
+            if ($nugetExe.VersionInfo.FileVersionRaw -gt '5.11')
+            {
                 $hasNuget = $true
             }
         }
@@ -67,11 +79,13 @@ Invoke-LabCommand -ActivityName 'Bootstrap NuGet.exe' -ComputerName $devOpsServe
 
         Invoke-WebRequest -Uri 'https://aka.ms/psget-nugetexe' -OutFile $nugetPathCurrentUser -ErrorAction Stop
 
-        if (Test-Path -Path $nugetPathCurrentUser) {
+        if (Test-Path -Path $nugetPathCurrentUser)
+        {
             $nugetExe = Get-Item -Path $nugetPathCurrentUser -ErrorAction SilentlyContinue
             Write-Host "'nuget.exe' exist in '$nugetPathCurrentUser' with version '$($nugetExe.VersionInfo.FileVersionRaw)'"
 
-            if ($nugetExe.VersionInfo.FileVersionRaw -lt '5.11') {
+            if ($nugetExe.VersionInfo.FileVersionRaw -lt '5.11')
+            {
                 Write-Host "'nuget.exe' has the version '$($nugetExe.VersionInfo.FileVersionRaw)' and needs to be updated."
                 Invoke-WebRequest -Uri 'https://aka.ms/psget-nugetexe' -OutFile $nugetPathCurrentUser -ErrorAction Stop
             }
@@ -88,23 +102,23 @@ Invoke-LabCommand -ActivityName 'Bootstrap NuGet.exe' -ComputerName $devOpsServe
     }
 
     Copy-Item -Path $nugetExe -Destination $nugetPath
-    
+
     [System.Environment]::SetEnvironmentVariable('Path', $env:Path + ';C:\NuGet', 'Machine')
 }
 
 Copy-LabFileItem -Path $PSScriptRoot\LabData\gittools.gitversion-5.0.1.3.vsix -ComputerName $devOpsServer
 
 Invoke-LabCommand -ActivityName "Upload and install 'GitVersion' extension" -ComputerName $devOpsServer -ScriptBlock {
-    $publisher = "GitTools"
-    $extension = "GitVersion"
+    $publisher = 'GitTools'
+    $extension = 'GitVersion'
     $version = '5.0.1.3'
     $vsix = 'C:\gittools.gitversion-5.0.1.3.vsix'
 
-    $param =  @{
-        Uri = "https://$($devOpsHostName):$devOpsPort/_apis/gallery/extensions?api-version=3.0-preview.1"
-        Credential = $devOpsCred    
-        Body = '{{"extensionManifest": "{0}"}}' -f ([Convert]::ToBase64String([IO.File]::ReadAllBytes($vsix)))
-        Method  = 'POST'
+    $param = @{
+        Uri         = "https://$($devOpsHostName):$devOpsPort/_apis/gallery/extensions?api-version=3.0-preview.1"
+        Credential  = $devOpsCred
+        Body        = '{{"extensionManifest": "{0}"}}' -f ([Convert]::ToBase64String([IO.File]::ReadAllBytes($vsix)))
+        Method      = 'POST'
         ContentType = 'application/json'
     }
     if ($PSVersionTable.PSEdition -eq 'Core')
@@ -112,14 +126,14 @@ Invoke-LabCommand -ActivityName "Upload and install 'GitVersion' extension" -Com
         $param.Add('SkipCertificateCheck', $true)
     }
 
-    $result = (Invoke-RestMethod @param)
-    
+    $null = (Invoke-RestMethod @param)
+
     Start-Sleep -Seconds 10
 
-    $param =  @{
-        Uri = "https://$($devOpsHostName):$devOpsPort/$collectionName/_apis/extensionmanagement/installedextensionsbyname/$publisher/$extension/$($version)?api-version=5.0-preview.1"
-        Credential = $devOpsCred        
-        Method  = 'POST'
+    $param = @{
+        Uri         = "https://$($devOpsHostName):$devOpsPort/$collectionName/_apis/extensionmanagement/installedextensionsbyname/$publisher/$extension/$($version)?api-version=5.0-preview.1"
+        Credential  = $devOpsCred
+        Method      = 'POST'
         ContentType = 'application/json'
     }
     if ($PSVersionTable.PSEdition -eq 'Core')
@@ -127,14 +141,14 @@ Invoke-LabCommand -ActivityName "Upload and install 'GitVersion' extension" -Com
         $param.Add('SkipCertificateCheck', $true)
     }
 
-    $result = (Invoke-RestMethod @param)
+    $null = (Invoke-RestMethod @param)
 
 } -Variable (Get-Variable -Name devOpsCred, devOpsHostName, devOpsPort, collectionName)
 
 Invoke-LabCommand -ActivityName 'Set Repository and create Build Pipeline' -ScriptBlock {
 
-    Set-Location -Path C:\Git\CommonTasks2
-    git checkout dev *>$null
+    Set-Location -Path C:\Git\CommonTasks
+    git checkout main *>$null
     Remove-Item -Path '.\azure-pipelines.yml'
     (Get-Content -Path '.\azure-pipelines On-Prem.yml' -Raw) -replace 'RepositoryUri_WillBeChanged', $nugetFeed.NugetV2Url | Set-Content -Path .\azure-pipelines.yml
     (Get-Content -Path .\Resolve-Dependency.psd1 -Raw) -replace 'PSGallery', 'PowerShell' | Set-Content -Path .\Resolve-Dependency.psd1
@@ -145,7 +159,7 @@ Invoke-LabCommand -ActivityName 'Set Repository and create Build Pipeline' -Scri
 
 } -ComputerName $devOpsServer -Variable (Get-Variable -Name nugetFeed)
 
-Write-Host 'Restarting Azure DevOps Server and worker machine...' -NoNewLine
+Write-Host 'Restarting Azure DevOps Server and worker machine...' -NoNewline
 Restart-LabVM -ComputerName (Get-LabVM -Role AzDevOps, HyperV) -Wait -NoDisplay
 Write-Host 'done'
 
