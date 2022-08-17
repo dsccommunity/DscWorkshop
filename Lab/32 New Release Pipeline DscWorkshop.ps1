1cee0dca382c75995c3a2bece0d05a88dd42994e
Raimund Andrée [MSFT] <r.andree@live.com>
2022-08-17 10:10:26 +0200

    Fixing lab deployment and pinning resource versions (#139)
    * Added 'DscResource.DocGenerator'
    
    * Small fixed
    
    * Fixing the version numbers
    
    * Added 'AllowOldPowerShellGetModule' to 'Resolve-Dependency.psd1'
    
    * Renamed according to new module name

--- Lab/32 New Release Pipeline DscWorkshop.ps1
+++ Lab/32 New Release Pipeline DscWorkshop.ps1
@@ -37,6 +37,11 @@ Invoke-LabCommand -ActivityName 'Set RepositoryUri and create Build Pipeline' -S
     Remove-Item -Path '.\azure-pipelines.yml'
     (Get-Content -Path '.\azure-pipelines On-Prem.yml' -Raw) -replace 'RepositoryUri_WillBeChanged', $nugetFeed.NugetV2Url | Set-Content -Path .\azure-pipelines.yml
     (Get-Content -Path .\Resolve-Dependency.psd1 -Raw) -replace 'PSGallery', 'PowerShell' | Set-Content -Path .\Resolve-Dependency.psd1
+
+    $content = [System.Collections.ArrayList](Get-Content -Path .\Resolve-Dependency.psd1)
+    $content.Insert(3, '    AllowOldPowerShellGetModule = $true # this is still required because of dependencies not following semantic versioning')
+    $content | Set-Content -Path .\Resolve-Dependency.psd1
+
     (Get-Content -Path .\RequiredModules.psd1 -Raw) -replace 'PSGallery', 'PowerShell' | Set-Content -Path .\RequiredModules.psd1
     git add .
     git commit -m 'Set RepositoryUri and create Build Pipeline'
