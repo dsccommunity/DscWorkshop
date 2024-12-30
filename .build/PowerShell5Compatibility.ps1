cc641098d365c8554d3f557f2329cd14d88fd172
Raimund Andree <r.andree@live.com>
2024-12-30 14:58:48 +0100

    Change error action to 'SilentlyContinue' when removing PSDesiredStateConfiguration module

--- .build/PowerShell5Compatibility.ps1
+++ .build/PowerShell5Compatibility.ps1
@@ -1,6 +1,6 @@
 task PowerShell5Compatibility -if ($PSVersionTable.PSEdition -eq 'Desktop') {
 
-    Remove-Item -Path $requiredModulesPath\PSDesiredStateConfiguration -ErrorAction Stop -Recurse -Force
+    Remove-Item -Path $requiredModulesPath\PSDesiredStateConfiguration -ErrorAction SilentlyContinue -Recurse -Force
     Write-Warning "'PSDesiredStateConfiguration' > 2.0 module is not supported on Windows PowerShell and not required for DSC compilation."
     Write-Warning "'PSDesiredStateConfiguration' was removed from the 'RequiredModules' folder."
 
